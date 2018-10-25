f61efb7b047747a825c80f3538cc708c9bb9aaa0
Chenna Keshava B S <16co108.keshava@nitk.edu.in>
2018-10-25 18:27:37 +0530

    added selection random private secret

--- generateMessage.py
+++ generateMessage.py
@@ -1,27 +1,29 @@
 import constants
 import generatePrivateSecret as getSecret
+import generateRandomString as getString
+from uuid import getnode as get_mac
 
 def generateMessage():
 
     print("\n\n\nGenerating Message\n\n\n")
 
 
-    # get the MAC address of the Local Machine
-    mac_addr = ''
+    # get the MAC address of the Local Machine, this is a 48-bit integer
+    mac_addr = get_mac()
 
     # generate a random k bit string
     k = constants.RANDOM_STRING_LENGTH
-    random_string = ''
+    random_string = getString.generateRandomString(constants.RANDOM_STRING_LENGTH)
 
     privateSecret = str(getSecret.generatePrivateSecret())
 
-    msg = mac_addr + privateSecret + random_string
+    msg = str(mac_addr) + privateSecret + random_string
 
-    print("MAC Address = {}".format(mac_addr))
-    print("Random_String = {}".format(random_string))
-    print("Private Secret = {}".format(privateSecret))
+    print("DEBUG: MAC Address = {}".format(mac_addr))
+    print("DEBUG: Random_String = {}".format(random_string))
+    print("DEBUG: Private Secret = {}".format(privateSecret))
 
-    print("Message: {}".format(msg))
+    print("DEBUG: Message: {}".format(msg))
 
     return msg
 
