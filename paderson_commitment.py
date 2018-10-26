1540b4cc09b183fe854c00abed25454bae80a7d7
16CO115 <16CO115@example.com>
2018-10-26 23:28:56 +0530

    added method to generate the cryptographic field

---
+++ paderson_commitment.py
@@ -0,0 +1,25 @@
+from Crypto.Util import number
+import random
+import constants
+
+# generates the cryptographic field (p,g,q)
+def generate_p_g_q(length_of_message=0):
+    
+    q = number.getPrime(max(constants.LENGTH_OF_PRIME, length_of_message + 1))
+
+    # p is a strong (k + 1)-bit prime
+    p = 2 * q + 1
+
+    # g is a random generator of G = QR((Z^*)_p)  QR = Quadratic Residues
+    # here g=2 sufficies since p is a safe prime.
+    g = 2  
+    
+    # # actual algorithm to find g
+    # for i in range(2,p):
+    #     if (i**((p-1)/2) % p) == 1:  # based on Euclid's Algorithm
+    #         g = i
+    #         break
+    
+    return p,g,q
+
+
