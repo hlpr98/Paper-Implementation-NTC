afe46297bb2f3717db4166d005833cde099646d7
16CO115 <16CO115@example.com>
2018-10-26 23:30:08 +0530

    added method to generate public key

--- paderson_commitment.py
+++ paderson_commitment.py
@@ -27,4 +27,9 @@ def generate_a():
 
     return random.randint(constants.RAND_NUMBER_LOWER_BOUND, constants.RAND_NUMBER_UPPER_BOUND)
 
+# generating the public keys 'h'; h = g^a mod p
+def generate_h(g,a,p):
+
+    return pow(g,a,p)   # g^a mod p
+
 
