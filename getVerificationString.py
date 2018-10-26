369cd7972c39dcd079d27a154f547ec3fad3f336
16CO115 <16CO115@example.com>
2018-10-27 01:52:02 +0530

    added a module for strig verification stage

--- getVerificationString.py
+++ getVerificationString.py
@@ -1,3 +1,14 @@
-
+# This fuction returns the verification string
+# here the verification string = XOR(messag_from_A, message_of_B)
 def getVerificationString(private_message_A, private_message_B):
-    pass
+    
+    # getting hexadecimal equivalent of messages
+    hex_msg_from_A = int(private_message_A, base=36)
+    hex_msg_from_B = int(private_message_B, base=36)
+
+    # XORing the two values
+    XOR = hex_msg_from_A ^ hex_msg_from_B
+
+    # returning hexadicimal equivalant of XOR
+    return hex(XOR)
+
