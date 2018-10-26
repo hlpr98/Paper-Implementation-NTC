da01c52381612c0a4dde40146204bfb02fbe3f08
16CO115 <16CO115@example.com>
2018-10-27 01:53:22 +0530

    added a module to extract message from commitment

--- getPrivateMessageFromNodeA.py
+++ getPrivateMessageFromNodeA.py
@@ -1,2 +1,20 @@
+import paderson_commitment
+import json
+import constants
+
+# returns the message encoded with commitment iff it was really sent be A
+# else returns None
 def getPrivateMessageFromNodeA(commit_value_from_A, open_value_from_A):
-    pass
+    open_value = json.loads(open_value_from_A)
+    message_from_A = open_value['message']
+    r_from_A = open_value['r']
+    h = open_value['h']
+
+    # here we convert the messages into integer equivalents for easy processing
+    commit_msg_after_opening = paderson_commitment.open(int(message_from_A, base=36), constants.SHARED_BASE, h, r_from_A, constants.SHARED_PRIME, commit_value_from_A)
+
+    if commit_msg_after_opening is not None:
+        return message_from_A
+    else:
+        return 'a'
+    
